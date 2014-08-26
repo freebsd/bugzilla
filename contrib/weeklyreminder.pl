037eed8b45f0cc6014d6746ac6c8aa820d849094
Marcus von Appen <marcus@sysfault.org>
2014-08-26 23:20:09 +0200

    - Only send reminder mails for bugs that need immediate attention. Users can   use the whining feature to get scheduled reports about all bugs. - Take the last modification timestamp for bugs into account. Onyl bugs that   were not touched for more than X days will be considered in the reminder.

--- contrib/weeklyreminder.pl
+++ contrib/weeklyreminder.pl
@@ -11,41 +11,51 @@ use Email::MIME;
 my $MAIL = "
 From: bz-noreply\@FreeBSD.org
 To: %s
-Subject: Current problem reports assigned to %s
+Subject: Problem reports assigned to %s that need special attention
 
 To view an individual PR, use:
-  https://bugs.freebsd.org/bugzilla/show_bug.cgi?id=(number).
+  https://bugs.freebsd.org/bugzilla/show_bug.cgi?id=(Bug Id).
 
-To view all PRs that are current assigned to you, use:
-  https://bugs.freebsd.org/bugzilla/buglist.cgi?assigned_to=%s&resolution=---.
-
-The following is a listing of current problems submitted by FreeBSD users.
-These represent problem reports covering all versions including
-experimental development code and obsolete releases.
+The following is a listing of current problems submitted by FreeBSD users,
+which need special attention. These represent problem reports covering
+all versions including experimental development code and obsolete releases.
 
 Status          |    Bug Id | Description
 ----------------+-----------+-------------------------------------------------
 %s
-%d problems total.
+%d problems total for which you should take action.
 ";
 
 my $TBLROW = "%-15s | %9s | %-48.47s\n";
+
+my $WAIT = 7;
 
 # We're a non-interactive user
 Bugzilla->usage_mode(USAGE_MODE_CMDLINE);
 # Get the db conection and query the db directly.
 my $dbh = Bugzilla->dbh;
 
+my $dnow = $dbh->sql_to_days("NOW()");
+my $query = q{
+SELECT
+  bugs.bug_id, bugs.short_desc, bugs.bug_status, p.login_name
+FROM
+  bugs
+  JOIN bug_status bs ON (bs.value = bugs.bug_status AND bs.is_open != 0)
+  JOIN profiles p ON (p.userid = bugs.assigned_to AND p.is_enabled = 1)
+WHERE
+  bugs.bug_status in (?, ?, ?, ?)
+  AND } . $dnow . " - " . $dbh->sql_to_days("bugs.delta_ts") . " >= " . $WAIT .
+" ORDER BY bugs.assigned_to, bugs.bug_status, bugs.bug_id;";
+
 my $openbugs = $dbh->selectall_arrayref(
-    "SELECT
-       bugs.bug_id, bugs.short_desc, bugs.bug_status, p.login_name
-     FROM
-       bugs
-       JOIN bug_status bs ON (bs.value = bugs.bug_status AND bs.is_open != 0)
-       JOIN profiles p ON (p.userid = bugs.assigned_to AND p.is_enabled = 1)
-     ORDER BY
-       bugs.assigned_to, bugs.bug_status, bugs.bug_id;"
-);
+    $query,
+    undef,
+    'Approval Needed',
+    'Feedback Needed',
+    'Needs MFC',
+    'Patch Ready');
+
 # If there are no bugs (hah!), exit gracefully
 if (scalar(@$openbugs) == 0) {
     exit 0;
@@ -74,11 +84,10 @@ foreach my $mail (keys %bugs) {
         $MAIL,
         $mail, # To
         $mail, # Subject
-        $mail, # bugzilla link
         $tblbugs,
         $bugcount
         );
     # Send the mail via the bugzilla configuration now.
-    #MessageToMTA($mailmsg, 1);
-    print($mailmsg);
+    MessageToMTA($mailmsg, 1);
+    #print($mailmsg);
 }
