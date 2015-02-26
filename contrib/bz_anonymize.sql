30c0c1aa02ea342d816cb06f8cb054560c656592
Marcus von Appen <marcus@sysfault.org>
2015-02-26 10:18:07 +0100

    - Improved anyonymizer script

--- contrib/bz_anonymize.sql
+++ contrib/bz_anonymize.sql
@@ -1,46 +1,53 @@
--- SET SCHEMA 'whatever';
+SET SCHEMA 'invalid';
 
-BEGIN;
+DO $$
+DECLARE
+    reassign integer;
+    fbsdids  integer[];
+BEGIN
+    SELECT userid INTO reassign
+        FROM profiles
+        WHERE login_name = 'nobody@FreeBSD.org';
+    SELECT array_agg(userid) INTO fbsdids
+        FROM (
+              SELECT userid
+              FROM profiles
+              WHERE LOWER(login_name) LIKE '%@freebsd.org'
+        ) as tmp;
+
+
+    UPDATE attachments SET submitter_id = reassign WHERE NOT (submitter_id = ANY(fbsdids));
     DELETE FROM audit_log;
-    UPDATE attach_data SET thedata=E'test';
-    UPDATE attachments SET description='test';
-    UPDATE bugs SET short_desc=concat('bug desc', bug_id);
-    UPDATE bugs SET alias=concat('bug alias', bug_id)
-        WHERE alias IS NOT NULL;
-    UPDATE bugs_activity set added='test' WHERE fieldid IN (
-        SELECT id from fielddefs WHERE name NOT IN ('bug_id',
-            'classification', 'product', 'version', 'rep_platform',
-            'op_sys', 'bug_status', 'status_whiteboard', 'keywords',
-            'resolution', 'bug_severity', 'priority', 'component',
-            'dependson', 'blocked', 'attachments.mimetype',
-            'attachments.ispatch', 'attachments.isobsolete',
-            'attachments.isprivate', 'attachments.submitter',
-            'target_milestone', 'creation_ts', 'delta_ts',
-            'longdescs.isprivate', 'longdescs.count', 'everconfirmed',
-            'estimated_time', 'remaining_time', 'deadline',
-            'flagtypes.name', 'work_time', 'percentage_complete',
-            'owner_idle_time', 'cf_type', 'days_elapsed')
-        );
-    UPDATE bugs_fulltext SET short_desc='test',
-                             comments='test',
-                             comments_noprivate='test';
+    UPDATE bugs SET reporter = reassign WHERE NOT (reporter = ANY(fbsdids));
+    UPDATE bugs set assigned_to = reassign WHERE NOT (assigned_to = ANY(fbsdids));
+    UPDATE bugs_activity SET who = reassign WHERE NOT (who = ANY(fbsdids));
+    DELETE FROM cc;
+    DELETE FROM component_cc;
+    UPDATE components SET initialowner = reassign WHERE NOT (initialowner = ANY(fbsdids));
+    DELETE FROM email_setting;
+    UPDATE flags set setter_id = reassign WHERE NOT (setter_id = ANY(fbsdids));
+    UPDATE flags set requestee_id = reassign WHERE NOT (requestee_id = ANY(fbsdids));
     DELETE FROM login_failure;
     DELETE FROM logincookies;
-    UPDATE longdescs set thetext='test';
+    UPDATE longdescs set who = reassign WHERE NOT (who = ANY(fbsdids));
     DELETE FROM namedqueries_link_in_footer;
     DELETE FROM namedquery_group_map;
     DELETE FROM namedqueries;
     DELETE FROM profile_search;
     DELETE FROM profile_setting;
-    -- Change cryptpassword to whatever you like
-    UPDATE profiles set
-        login_name=concat('user', userid, '@test'),
-        cryptpassword='',
-        realname=concat('User ', userid),
-        extern_id=NULL;
+    DELETE FROM profiles_activity;
+    DELETE FROM profiles WHERE NOT (userid = ANY(fbsdids));
+    UPDATE quips SET userid = reassign WHERE NOT (userid = ANY(fbsdids));
+    DELETE FROM reports;
+    DELETE FROM series_categories;
+    DELETE FROM series_data;
+    DELETE FROM series;
+    UPDATE tag set user_id = reassign WHERE NOT (user_id = ANY(fbsdids));
     DELETE FROM tokens;
-    DELETE FROM whine_queries;
+    DELETE FROM user_group_map WHERE NOT (user_id = ANY(fbsdids));
+    DELETE FROM watch;
     DELETE FROM whine_schedules;
+    DELETE FROM whine_queries;
     DELETE FROM whine_events;
-ROLLBACK;
--- replace the ROLLBACK with a COMMIT.
+END $$;
+
