03332aa963f91ebdc272cd4b4cc4fa703086efb0
Mark Linimon <linimon@FreeBSD.org>
2024-01-11 13:34:32 +0000

    Bring weeklyreminder.pl up to date with production as of early 2023.

--- weeklyreminder.pl
+++ weeklyreminder.pl
@@ -2,10 +2,12 @@
 
 use strict;
 use warnings;
+use lib qw(. lib);
 
 use Bugzilla;
 use Bugzilla::Mailer;
 use Bugzilla::Constants;
+use Bugzilla::Util;
 use Email::MIME;
 
 use constant {
@@ -13,13 +15,15 @@ use constant {
     WAIT => 7
 };
 
-my $MAIL = "From: bugzilla-noreply\@FreeBSD.org
-To: %s
-Subject: Problem reports for %s that need special attention
+my $urlbase = Bugzilla->params->{'urlbase'};
 
-To view an individual PR, use:
-  https://bugs.freebsd.org/bugzilla/show_bug.cgi?id=(Bug Id).
+my $MAIL_TXT_HEADER = "To view an individual PR, use:
+  ${urlbase}show_bug.cgi?id=(Bug Id).
+";
 
+my $MAIL_HTML_HEADER = '<pre style="font-family: monospace;">';
+
+my $MAIL_COMMON = "
 The following is a listing of current problems submitted by FreeBSD users,
 which need special attention. These represent problem reports covering
 all versions including experimental development code and obsolete releases.
@@ -30,7 +34,12 @@ Status      |    Bug Id | Description
 %d problems total for which you should take action.
 ";
 
+my $MAIL_TXT_FOOTER = "";
+
+my $MAIL_HTML_FOOTER = '</pre>';
+
 my $TBLROW = "%-11s | %9s | %-50.49s\n";
+my $TBLROW_HTML = "%-11s | <a href=\"%s\">%9s</a> | <a href=\"%s\">%s</a>\n";
 
 # We're a non-interactive user
 Bugzilla->usage_mode(USAGE_MODE_CMDLINE);
@@ -114,19 +123,58 @@ foreach my $bug (@$openbugs) {
 foreach my $mail (keys %bugs) {
     # Prep mail content
     my $tblbugs = "";
+    my $tblbugs_html = "";
     my $bugcount = scalar(@{$bugs{$mail}});
     foreach my $bug (@{$bugs{$mail}}) {
         my ($id, $desc, $status, $mail__, @assignedto) = @$bug;
         $tblbugs .= sprintf($TBLROW, $status, $id, $desc);
+        # we can not use %50s format on HTML-escaped string
+        # sut it implicitly
+        my $desc_cut = html_quote(substr($desc, 0, 49));
+        my $url = "${urlbase}show_bug.cgi?id=$id";
+        $tblbugs_html .= sprintf($TBLROW_HTML, $status, $url, $id, $url, $desc_cut);
+        $tblbugs_html =~ s/(<a href=".*?">)(\s+)/$2$1/;
     }
-    my $mailmsg = Email::MIME->new(sprintf(
-        $MAIL,
-        $mail, # To
-        $mail, # Subject
-        $tblbugs,
-        $bugcount
-        ));
+
+    my $body_txt = $MAIL_TXT_HEADER;
+    $body_txt .= sprintf($MAIL_COMMON, $tblbugs, $bugcount);
+    $body_txt .= $MAIL_TXT_FOOTER;
+
+    my $body_html = $MAIL_HTML_HEADER;
+    $body_html .= sprintf($MAIL_COMMON, $tblbugs_html, $bugcount);
+    $body_html .= $MAIL_HTML_FOOTER;
+
+    # parts should go from least rich to most rich format
+    my @parts = (
+        Email::MIME->create(
+            attributes => {
+                content_type => "text/plain",
+                charset => "UTF-8"
+            },
+            body => $body_txt
+        ),
+        Email::MIME->create(
+            attributes => {
+                content_type => "text/html",
+                charset => "UTF-8"
+            },
+            body => $body_html
+        ),
+    );
+
+    my $mailmsg = Email::MIME->create(
+        header_str => [
+            From => 'bugzilla-noreply@FreeBSD.org',
+            To => [ $mail ],
+            Subject => "Problem reports for $mail that need special attention"
+        ],
+        attributes => {
+            content_type => "multipart/alternative"
+        },
+        parts      => [ @parts ],
+    );
+
     # Send the mail via the bugzilla configuration.
+    # print $mailmsg->as_string;
     MessageToMTA($mailmsg, 1);
-    #print($mailmsg);
 }
