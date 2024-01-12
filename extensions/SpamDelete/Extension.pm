a20e43e7ab26274ebe2b30e28494a6203ef84ab1
Mark Linimon <linimon@FreeBSD.org>
2024-01-12 02:59:54 +0000

    Catch Extension.pm up with production 20210521: add whitelist; add _get_comments_with_link .

--- extensions/SpamDelete/Extension.pm
+++ extensions/SpamDelete/Extension.pm
@@ -14,7 +14,39 @@ use Bugzilla::User;
 use Bugzilla::Util;
 use Bugzilla::Extension::BFBSD::Helpers;
 
+use Date::Parse;
+use experimental 'smartmatch';
+
 our $VERSION = '0.1.0';
+my @WHITELIST = qw(
+    github.com
+    bugs.freebsd.org
+    freebsd.org
+    forums.freebsd.org
+    git.freebsd.org
+    cgit.freebsd.org
+    reviews.freebsd.org
+    lists.freebsd.org
+    bugs.freebsd.org
+    docs.freebsd.org
+    www.freebsd.org
+    wiki.freebsd.org
+    svn.freebsd.org
+    security.freebsd.org
+    svnweb.freebsd.org
+    download.freebsd.org
+    gnu.org
+    www.gnu.org
+    bitbucket.org
+    bz-attachments.freebsd.org
+);
+
+sub is_white_listed {
+    my $url = shift;
+    $url = lc($url);
+    $url =~ s@^https?://([^/]*).*@$1@;
+    return ($url ~~ @WHITELIST);
+}
 
 sub install_update_db {
     my ($self, $args) = @_;
@@ -32,7 +64,7 @@ sub page_before_template {
     my ($self, $args) = @_;
     my $page = $args->{page_id};
 
-    if ($page ne "searchspam.html" && $page ne "deletespam.html") {
+    if ($page ne "searchspam.html" && $page ne "deletespam.html" && $page ne "listsuspects.html") {
         return;
     }
 
@@ -46,9 +78,98 @@ sub page_before_template {
     }
     if ($page eq "searchspam.html") {
         _search_user($args);
+    } elsif ($page eq "listsuspects.html") {
+        _list_suspects($args);
     } elsif ($page eq "deletespam.html") {
         _block($args);
     }
+}
+
+sub _get_comments_with_link {
+    my $dbh = Bugzilla->dbh;
+    my $input = Bugzilla->input_params;
+
+    my $only_suspects = 0;
+    my $time_limit = "(bug_when > NOW() - INTERVAL '2 weeks')";
+    my @bind;
+    if (defined($input->{year})) {
+        if ($input->{year} =~ m/^(\d+)$/) {
+            push @bind, $1;
+            $time_limit = "(bug_when > NOW() - INTERVAL '7 years')";
+            $time_limit = "(EXTRACT(YEAR from bug_when) = ?)";
+            $only_suspects = 1;
+        }
+    }
+    my $sth = $dbh->prepare("
+        SELECT bug_id, comment_id, who, login_name, bug_when, thetext
+        FROM longdescs JOIN profiles ON (userid = who)
+        WHERE $time_limit 
+            AND (thetext ilike '%http://%' or thetext ilike '%https://%')
+            AND NOT (login_name ilike '%\@freebsd.org')
+            AND (disabledtext = '')
+    ");
+    $sth->execute(@bind);
+
+    my $age_sth = $dbh->prepare("
+        SELECT profiles_when
+        FROM profiles_activity WHERE fieldid=30 and who=? LIMIT 1
+    ");
+
+    my @comments;
+    my %registered;
+    while (my($bug, $comment, $who, $login, $when, $text) = $sth->fetchrow_array) {
+        my @links;
+        while ($text =~ m@(https?://.*?)(?:\h|$)@mig) {
+            push @links, $1;
+        }
+        @links = grep { not is_white_listed($_) } @links;
+        next unless(@links);
+        if (!defined($registered{$who})) {
+            $registered{$who} = '';
+            $age_sth->execute($who);
+            if (my ($activity_when) = $age_sth->fetchrow_array) {
+                $registered{$who} = $activity_when;
+            }
+        }
+        my $age = 'Unknown';
+        my $suspect = 0;
+        if ($registered{$who} ne '') {
+            my $time_reg = str2time($registered{$who});
+            my $time_comment = str2time($when);
+            my $diff = $time_comment - $time_reg;
+            if ($diff < 3600) {
+                $age = int($diff/60);
+                $age .= ' minutes';
+                $suspect = 1;
+            }
+            elsif ($diff < 3600*24) {
+                $age = int($diff/3600);
+                $age .= ' hours';
+                $suspect = 1;
+            }
+            elsif ($diff < 3600*24*7) {
+                $age = int($diff/(3600*24));
+                $age .= ' days';
+            }
+            else {
+                $age = int($diff/(3600*24*7));
+                $age .= ' weeks';
+            }
+        }
+        next if ($only_suspects and not $suspect);
+        my $entry = {
+            'bug' => $bug,
+            'when' => $when,
+            'who' => $login,
+            'registered' => $registered{$who},
+            'age' => $age,
+            'suspect' => $suspect,
+            'links' => \@links
+        };
+        push @comments, $entry;
+    }
+
+    return [ sort { $b->{when} cmp $a->{when} } @comments ];
 }
 
 sub _get_bugs {
@@ -89,6 +210,14 @@ sub _search_user {
     $vars->{bugs} = _get_bugs($spamuser);
 }
 
+sub _list_suspects {
+    my $args = shift();
+    my $vars = $args->{vars};
+    my $cgi = Bugzilla->cgi;
+
+    $vars->{comments} = _get_comments_with_link();
+}
+
 sub _block {
     my $args = shift();
     my $vars = $args->{vars};
@@ -127,7 +256,8 @@ sub _block {
 
 sub _send_info {
     my ($who, $spamuser) = @_;
-    my $mail = "From: bugzilla-noreply\@FreeBSD.org
+    my $mail = "
+From: bugzilla-noreply\@FreeBSD.org
 To: %s
 Subject: Spammer blocked on %s
 
