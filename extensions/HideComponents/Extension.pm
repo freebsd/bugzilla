94406887a2aac24cf0137bcedd869aaf1036167a
Mark Linimon <linimon@FreeBSD.org>
2024-01-26 01:19:18 +0000

    From development: modify what ports Components non-developers can use.
    This is currently a duplicate of what is in BFBSD.

---
+++ extensions/HideComponents/Extension.pm
@@ -0,0 +1,27 @@
+package Bugzilla::Extension::HideComponents;
+
+use strict;
+use warnings;
+
+use base qw(Bugzilla::Extension);
+
+our $VERSION = '0.1.0';
+
+
+sub template_before_process {
+    my ($self, $args) = @_;
+    my ($vars, $file) = @$args{qw(vars file)};
+
+    return if $file ne 'bug/create/create.html.tmpl';
+    my $user = Bugzilla->user;
+    # Limit noise from mis-classified PRs by non-committer
+    # https://bugs.freebsd.org/bugzilla/show_bug.cgi?id=198411
+    if (!$user->in_group('freebsd_committe')) {
+        $vars->{hide_components} = [
+            'Package Infrastructure',
+            'Ports Framework'
+        ];
+    }
+}
+
+__PACKAGE__->NAME;
